a3ea71ae3facdb9f585a645a04c855efc3745c3a
johnkennedy12 <133625685+johnkennedy12@users.noreply.github.com>
2023-06-17 10:48:00 -0400

    Create group1file.sh

---
+++ group1file.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo ’A simple arithmetic script.’
+num1=4
+num2=12
+# Check which number is greater
+
+if [ $num1 -gt $num2 ]
+then
+    echo ‘$num1 is greater than $num2.’
+else
+    echo ‘$num2 is greater than $num1.’
+fi
+
+
+A. The ability for Jenkins to pull code from GitHub
+B. The ability for GitHub to trigger Jenkins build jobs 
